c2946c6df2f15eebcef196352a57643ff2d617f8
Allyson Rodrigues <125301945+Rodrigues4567@users.noreply.github.com>
2024-04-24 03:17:41 -0300

    Projeto landing page restaurante pronta!

--- main.js
+++ main.js
@@ -11,3 +11,15 @@ responsiveBtn.addEventListener('click', menuShow = () => {
         responsiveMenu.classList.add('open');
     }
 })
+
+window.addEventListener('scroll', headerShadow = () => {
+    let header = document.querySelector('.header-container');
+    let scrollPosition = window.scrollY;
+
+    if (scrollPosition > 0) {
+        header.style.boxShadow = '5px 1px 5px rgba(0, 0, 0, 0.150)';
+    }
+    else {
+        header.style.boxShadow = 'none';
+    }
+})
