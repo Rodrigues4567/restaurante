6e7ed2e85b0fdc0214d8bfc43598da342dbdba80
Allyson Rodrigues <125301945+Rodrigues4567@users.noreply.github.com>
2024-04-24 01:11:16 -0300

    Versão responsiva pronta!

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 
+const responsiveBtn = document.querySelector('.responsive-btn');
+
+responsiveBtn.addEventListener('click', menuShow = () => {
+    let responsiveMenu = document.querySelector('.responsive-options');
+
+    if (responsiveMenu.classList.contains('open')) {
+        responsiveMenu.classList.remove('open');
+    }
+    else {
+        responsiveMenu.classList.add('open');
+    }
+})
